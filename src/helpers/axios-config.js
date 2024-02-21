f837e116b2ad1c32b51144aad4321821e4fc80bc
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2024-02-21 15:16:43 +0200

    Update axios-config.js

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // axios.defaults.baseURL = "https://king-prawn-app-hnaei.ondigitalocean.app";
 axios.defaults.headers.common['Accept'] = 'application/json';
-axios.defaults.baseURL = 'http://127.0.0.1:3000/api';
+axios.defaults.baseURL = 'https://iteen-back.onrender.com/api';
 
 axios.defaults.headers.authorization = 'Bearer ' + localStorage.getItem('booking');
 
