197c41c6cb94aa7ff93af0c9dea263e63a51a4f3
monyshchenko.laptop <kekymber@gmail.com>
2024-04-19 14:31:06 +0300

    added soft-tech switch to my subroup creating bu mentor

--- src/components/modals/ChangeManagerCourses/ChangeManagerCourses.jsx
+++ src/components/modals/ChangeManagerCourses/ChangeManagerCourses.jsx
@@ -1,157 +1,155 @@
 import styles from './ChangeManagerCourses.module.scss';
 import teacherStyles from '../../../styles/teacher.module.scss';
 import Modal from '../../Modal/Modal';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Switch from 'react-switch';
 import {
-  getTeacherCourses,
-  postTeacherCourse,
-  deleteTeacherCourse,
-  patchTeacherCourse
+ getTeacherCourses,
+ postTeacherCourse,
+ deleteTeacherCourse,
+ patchTeacherCourse,
 } from '../../../helpers/course/course';
-import {useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
-  addTeacherCourses,
-  setTeacherCourses,
-  updateTeacherCourse
+ addTeacherCourses,
+ setTeacherCourses,
+ updateTeacherCourse,
 } from '../../../redux/action/course.action';
-import {getTeacherTypes} from '../../../helpers/teacher/teacher-type';
 
 const ChangeManagerCourses = ({
-  isOpen,
-  handleClose,
-  teacherId,
-  filteringCourses,
-  setFilteringCourses,
-  forFilters = false
+ isOpen,
+ handleClose,
+ teacherId,
+ filteringCourses,
+ setFilteringCourses,
+ forFilters = false,
 }) => {
-  const dispatch = useDispatch();
-  const courses = useSelector(state => state.courses.courses);
-  const teacherCourses = useSelector(state => state.courses.teacherCourses) || [];
-  const [teacherTypes, setTeacherTypes] = useState([]);
-  const [teacherTypesId, setTeacherTypesId] = useState({tech: 2, soft: 1});
-  // const fetchTeacherTypes = async () => {
-  //   const res = await getTeacherTypes();
-  //   setTeacherTypes(res.data);
-  //   if (teacherTypes.length > 0) {
-  //     setTeacherTypesId({
-  //       tech: teacherTypes.filter(type => type.type === 'tech')[0].id,
-  //       soft: teacherTypes.filter(type => type.type === 'soft')[0].id
-  //     });
-  //   }
-  // };
-  useEffect(() => {
-    const fetchAllCourses = async () => {
-      if (!forFilters) {
-        const teachersCrs = await getTeacherCourses(teacherId);
-        dispatch(setTeacherCourses(teachersCrs.data));
-      }
-    };
-    fetchAllCourses();
-    // fetchTeacherTypes();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [teacherId, dispatch]);
+ const dispatch = useDispatch();
+ const courses = useSelector((state) => state.courses.courses);
+ const teacherCourses = useSelector((state) => state.courses.teacherCourses) || [];
+ const [teacherTypes, setTeacherTypes] = useState([]);
+ const [teacherTypesId, setTeacherTypesId] = useState({ tech: 2, soft: 1 });
+ // const fetchTeacherTypes = async () => {
+ //   const res = await getTeacherTypes();
+ //   setTeacherTypes(res.data);
+ //   if (teacherTypes.length > 0) {
+ //     setTeacherTypesId({
+ //       tech: teacherTypes.filter(type => type.type === 'tech')[0].id,
+ //       soft: teacherTypes.filter(type => type.type === 'soft')[0].id
+ //     });
+ //   }
+ // };
+ useEffect(() => {
+  const fetchAllCourses = async () => {
+   if (!forFilters) {
+    const teachersCrs = await getTeacherCourses(teacherId);
+    dispatch(setTeacherCourses(teachersCrs.data));
+   }
+  };
+  fetchAllCourses();
+  // fetchTeacherTypes();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+ }, [teacherId, dispatch]);
 
-  const handleCheckboxChange = async (courseId, id) => {
-    if (!forFilters) {
-      if (teacherCourses.some(el => el.courseId === courseId)) {
-        await deleteTeacherCourse(id, courseId);
-        dispatch(setTeacherCourses(teacherCourses.filter(el => el.courseId !== courseId)));
-      } else {
-        const res = await postTeacherCourse(id, courseId);
+ const handleCheckboxChange = async (courseId, id) => {
+  if (!forFilters) {
+   if (teacherCourses.some((el) => el.courseId === courseId)) {
+    await deleteTeacherCourse(id, courseId);
+    dispatch(setTeacherCourses(teacherCourses.filter((el) => el.courseId !== courseId)));
+   } else {
+    const res = await postTeacherCourse(id, courseId);
 
-        dispatch(addTeacherCourses(res.data));
-      }
+    dispatch(addTeacherCourses(res.data));
+   }
+  } else {
+   setFilteringCourses((prevCourses) => {
+    if (prevCourses.includes(courseId)) {
+     return prevCourses.filter((el) => el !== courseId);
     } else {
-      setFilteringCourses(prevCourses => {
-        if (prevCourses.includes(courseId)) {
-          return prevCourses.filter(el => el !== courseId);
-        } else {
-          return [...prevCourses, courseId];
-        }
-      });
+     return [...prevCourses, courseId];
     }
-  };
-  // const [flag, setFlag] = useState(0)
-  const handleTeacherTypeChange = async ({element, teacherId, teacherCourse}) => {
-    const TeacherTypeId = element ? teacherTypesId['tech'] : teacherTypesId['soft'];
-    console.log(TeacherTypeId);
-    try {
-      await patchTeacherCourse(teacherId, teacherCourse.courseId, {TeacherTypeId});
-      dispatch(updateTeacherCourse({...teacherCourse, TeacherTypeId}));
-    } catch (error) {}
-  };
-  if (forFilters && !isOpen) return <></>;
-  return (
-    <>
-      <Modal open={isOpen} onClose={handleClose}>
-        <h1 className={styles.title}>Teacher courses</h1>
-        <h3 className={styles.managerLinkTitle}>
-          {forFilters
-            ? `Pick courses for filtering, and close this window
+   });
+  }
+ };
+ // const [flag, setFlag] = useState(0)
+ const handleTeacherTypeChange = async ({ element, teacherId, teacherCourse }) => {
+  const TeacherTypeId = element ? teacherTypesId['tech'] : teacherTypesId['soft'];
+  console.log(TeacherTypeId);
+  try {
+   await patchTeacherCourse(teacherId, teacherCourse.courseId, { TeacherTypeId });
+   dispatch(updateTeacherCourse({ ...teacherCourse, TeacherTypeId }));
+  } catch (error) {}
+ };
+ if (forFilters && !isOpen) return <></>;
+ return (
+  <>
+   <Modal open={isOpen} onClose={handleClose}>
+    <h1 className={styles.title}>Teacher courses</h1>
+    <h3 className={styles.managerLinkTitle}>
+     {forFilters
+      ? `Pick courses for filtering, and close this window
           to apply changes:`
-            : 'Choose courses'}
-          {/* Teacher :TODO: make href */}
-          {/* <a className={styles.managerLink} href={`/user/${managerInfo.id}/planning`}>
+      : 'Choose courses'}
+     {/* Teacher :TODO: make href */}
+     {/* <a className={styles.managerLink} href={`/user/${managerInfo.id}/planning`}>
             {managerInfo.name}
           </a> */}
-        </h3>
-        <div className={styles.coursesBox}>
-          {courses.length > 0 &&
-            courses.map(course => {
-              const teacherCourse = (teacherCourses || []).filter(
-                el => el.courseId === course.id
-              )[0];
-              console.log(course.id, teacherCourse);
-              return (
-                <div key={course.id} className={styles.checkBoxDiv}>
-                  <div className={styles.checkBoxLabel}>
-                    <input
-                      className={styles.checkBox}
-                      type="checkbox"
-                      key={Math.random() * 1000 - 10}
-                      checked={
-                        !forFilters
-                          ? teacherCourse && teacherCourse.courseId
-                          : (filteringCourses || []).length > 0 &&
-                            filteringCourses.some(el => {
-                              return el === course.id;
-                            })
-                      }
-                      onChange={() => handleCheckboxChange(course.id, teacherId)}
-                    />
-                    {course.name}
-                  </div>
-                  {!forFilters && teacherCourse && (
-                    <>
-                      {console.log(
-                        teacherCourse.TeacherTypeId,
-                        teacherCourse.TeacherTypeId === teacherTypesId['tech']
-                      )}
-                      <div className={styles.switch_wrapper}>
-                        <Switch
-                          key={Math.random() * 1000 - 10}
-                          checked={teacherCourse.TeacherTypeId === teacherTypesId['tech']}
-                          // checked={flag}
-                          className={teacherStyles.remove_svg_switch}
-                          // onChange={()=>{setFlag(!flag)}}
-                          onChange={element => {
-                            handleTeacherTypeChange({element, teacherId, teacherCourse});
-                          }}></Switch>
-                        <span className={teacherStyles.switch_label}>tech</span>
-                      </div>
-                    </>
-                  )}
-                </div>
-              );
-            })}
+    </h3>
+    <div className={styles.coursesBox}>
+     {courses.length > 0 &&
+      courses.map((course) => {
+       const teacherCourse = (teacherCourses || []).filter((el) => el.courseId === course.id)[0];
+       console.log(course.id, teacherCourse);
+       return (
+        <div key={course.id} className={styles.checkBoxDiv}>
+         <div className={styles.checkBoxLabel}>
+          <input
+           className={styles.checkBox}
+           type="checkbox"
+           key={Math.random() * 1000 - 10}
+           checked={
+            !forFilters
+             ? teacherCourse && teacherCourse.courseId
+             : (filteringCourses || []).length > 0 &&
+               filteringCourses.some((el) => {
+                return el === course.id;
+               })
+           }
+           onChange={() => handleCheckboxChange(course.id, teacherId)}
+          />
+          {course.name}
+         </div>
+         {!forFilters && teacherCourse && (
+          <>
+           {console.log(
+            teacherCourse.TeacherTypeId,
+            teacherCourse.TeacherTypeId === teacherTypesId['tech']
+           )}
+           <div className={styles.switch_wrapper}>
+            <Switch
+             key={Math.random() * 1000 - 10}
+             checked={teacherCourse.TeacherTypeId === teacherTypesId['tech']}
+             // checked={flag}
+             className={teacherStyles.remove_svg_switch}
+             // onChange={()=>{setFlag(!flag)}}
+             onChange={(element) => {
+              handleTeacherTypeChange({ element, teacherId, teacherCourse });
+             }}
+            ></Switch>
+            <span className={teacherStyles.switch_label}>tech</span>
+           </div>
+          </>
+         )}
         </div>
-        {/* <button className={styles.input__submit} onClick={handleSave}>
+       );
+      })}
+    </div>
+    {/* <button className={styles.input__submit} onClick={handleSave}>
           Save
         </button> */}
-      </Modal>
-    </>
-  );
+   </Modal>
+  </>
+ );
 };
 
 export default ChangeManagerCourses;
--- src/components/modals/NewMySubgroup/NewMySubgroup.jsx
+++ src/components/modals/NewMySubgroup/NewMySubgroup.jsx
@@ -1,91 +1,123 @@
 import Modal from '../../Modal/Modal';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import FormInput from '../../FormInput/FormInput';
 import Form from '../../Form/Form';
-import {bulkSlotCreate} from '../../../helpers/slot/slot';
-import {createSubgroupMentor} from '../../../helpers/subgroup/subgroup';
+import styles from './NewMySubgroup.module.scss';
+import { bulkSlotCreate } from '../../../helpers/slot/slot';
+import { createSubgroupMentor } from '../../../helpers/subgroup/subgroup';
 
-const NewMySubgroup = ({isOpen, handleClose, slots, info}) => {
-  const [schedule, setSchedule] = useState('');
-  console.log(info);
-  const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  const calculateSchedule = () => {
-    let tmpSchedule = '';
-    slots.forEach(slot => {
-      if (slot.rowSpan)
-        tmpSchedule += `${weekDays[slot.weekDay]}: ${slot.time} - ${slot.timeEnd}\n`;
-    });
-    setSchedule(tmpSchedule);
-  };
-  useEffect(() => {
-    if (isOpen) calculateSchedule();
-  }, [isOpen]);
+const NewMySubgroup = ({ isOpen, handleClose, slots, info }) => {
+ const [schedule, setSchedule] = useState('');
+ console.log(info);
+ const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+ const calculateSchedule = () => {
+  let tmpSchedule = '';
+  slots.forEach((slot) => {
+   if (slot.rowSpan) tmpSchedule += `${weekDays[slot.weekDay]}: ${slot.time} - ${slot.timeEnd}\n`;
+  });
+  setSchedule(tmpSchedule);
+ };
+ useEffect(() => {
+  if (isOpen) calculateSchedule();
+ }, [isOpen]);
 
-  return (
-    <>
-      {isOpen && (
-        <Modal open={isOpen} onClose={handleClose}>
-          <Form
-            onSubmit={() => {
-              handleClose();
-            }}
-            type={{type: 'post'}}
-            requests={{
-              post: () => {
-                bulkSlotCreate(slots);
-                createSubgroupMentor({
-                  subgroupId: info.subGroup.value,
-                  mentorId: info.userId,
-                  TeacherTypeId: info.selectedCourse.TeacherTypeId,
-                  schedule
-                });
-              }
-            }}
-            slots={JSON.stringify(slots)}
-            status={{
-              successMessage: 'Successfully created subgroup',
-              failMessage: 'Failed to create subgroup'
-            }}
-            title="New subgroup">
-            <FormInput
-              title="Name:"
-              type="text"
-              name="name"
-              max={50}
-              value={info.subGroup.label}
-              placeholder="Name"
-              isRequired={true}
-              disabled={1}
-            />
-            <FormInput
-              title="Schedule:"
-              type="text"
-              name="schedule"
-              max={50}
-              value={schedule}
-              placeholder="Name"
-              isRequired={true}
-              textArea={1}
-              disabled={1}
-            />
+ return (
+  <>
+   {isOpen && (
+    <Modal open={isOpen} onClose={handleClose}>
+     <Form
+      onSubmit={() => {
+       handleClose();
+      }}
+      type={{ type: 'post' }}
+      requests={{
+       post: () => {
+        bulkSlotCreate(slots);
+        createSubgroupMentor({
+         subgroupId: info.subGroup.value,
+         mentorId: info.userId,
+         TeacherTypeId: info.teacherType,
+         schedule,
+        });
+       },
+      }}
+      slots={JSON.stringify(slots)}
+      status={{
+       successMessage: 'Successfully created subgroup',
+       failMessage: 'Failed to create subgroup',
+      }}
+      title="New subgroup"
+     >
+      <FormInput
+       title="Name:"
+       type="text"
+       name="name"
+       max={50}
+       value={info.subGroup.label}
+       placeholder="Name"
+       isRequired={true}
+       disabled={1}
+      />{' '}
+      <div className={styles.input__block}>
+       <FormInput
+        title="Course:"
+        type="text"
+        name="course"
+        max={50}
+        value={info.selectedCourse.label}
+        placeholder="Course"
+        isRequired={true}
+        disabled={1}
+       />{' '}
+       <FormInput
+        title="Type:"
+        type="text"
+        name="type"
+        max={50}
+        value={info.teacherType === 2 ? 'Tech' : 'Soft'}
+        placeholder="Type"
+        isRequired={true}
+        disabled={1}
+       />{' '}
+      </div>{' '}
+      <FormInput
+       title="Schedule:"
+       type="text"
+       name="schedule"
+       max={50}
+       value={schedule}
+       placeholder="Name"
+       isRequired={true}
+       textArea={1}
+       disabled={1}
+      />{' '}
+      <div className={styles.input__block}>
+       <FormInput
+        classname="input__bottom"
+        title="Start:"
+        type="date"
+        name="startDate"
+        value={info.startDate}
+        isRequired={true}
+        disabled={true}
+       />
 
-            {/* <FormInput
-              title="Description:"
-              type="text"
-              name="description"
-              value={subgroup.description}
-              textArea={true}
-              placeholder="Description"
-              isRequired={true}
-              handler={e => {
-                setSubgroup({...subgroup, description: e});
-              }}
-            /> */}
-          </Form>
-        </Modal>
-      )}
-    </>
-  );
+       <FormInput
+        classname="input__bottom"
+        title="End:"
+        type="date"
+        name="EndDate"
+        disabled={true}
+        // if appointmentType is group +6 month, else +1
+        value={info.endDate}
+        isRequired={true}
+       />
+      </div>
+     </Form>
+    </Modal>
+   )}
+  </>
+ );
 };
 
 export default NewMySubgroup;
