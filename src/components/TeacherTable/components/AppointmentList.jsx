5376947e646837307ce4e2caa09b22ce1172afec
monyshchenko.laptop <kekymber@gmail.com>
2024-04-19 15:37:24 +0300

    added tooltip for teacher page

--- src/components/TeacherTable/components/AppointmentList.jsx
+++ src/components/TeacherTable/components/AppointmentList.jsx
@@ -1,84 +1,115 @@
-import React, {useEffect} from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useEffect } from 'react';
 
 import appointmentStyles from '../../../styles/appointment.module.scss';
-import {getAppointmentTypes} from '../../../helpers/teacher/appointment-type';
+import { getAppointmentTypes } from '../../../helpers/teacher/appointment-type';
+import 'react-tooltip/dist/react-tooltip.css';
+import { Tooltip } from 'react-tooltip';
 
 export default function AppointmentList({
-  setAppointmentTypes,
-  appointmentTypes,
-  user,
-  setSelectedAppointment
+ setAppointmentTypes,
+ appointmentTypes,
+ user,
+ setSelectedAppointment,
 }) {
-  useEffect(() => {
-    const fetchAppointmentType = async () => {
-      try {
-        const response = await getAppointmentTypes();
+ useEffect(() => {
+  const fetchAppointmentType = async () => {
+   try {
+    const response = await getAppointmentTypes();
 
-        setAppointmentTypes(
-          response.data.sort((a, b) => {
-            const order = ['replacement_group', 'replacement_private', 'free'];
-            return order.indexOf(a.name) - order.indexOf(b.name);
-          })
-        );
+    setAppointmentTypes(
+     response.data.sort((a, b) => {
+      const order = ['replacement_group', 'replacement_private', 'free'];
+      return order.indexOf(a.name) - order.indexOf(b.name);
+     })
+    );
 
-        if (appointmentTypes && appointmentTypes.length > 0) {
-          setSelectedAppointment({
-            id: appointmentTypes[0].id,
-            name: appointmentTypes[0].name
-          });
-        }
-      } catch (error) {
-        console.error('Error fetching appointment types:', error);
-      }
-    };
-
-    fetchAppointmentType();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const translateAppointmentTypeName = name => {
-    switch (name) {
-      case 'universal':
-        return 'Universal';
-      case 'appointed_group':
-        return 'Appointed group';
-      case 'appointed_private':
-        return 'Appointed private';
-      case 'appointed_junior_group':
-        return 'Appointed jun group';
-      case 'free':
-        return 'Remove';
-      case 'replacement_group':
-        return 'Group replacement';
-      case 'replacement_private':
-        return 'Private replacement';
-      case 'replacement_junior_group':
-        return 'Jun group replacement';
-      default:
-        return name;
+    if (appointmentTypes && appointmentTypes.length > 0) {
+     setSelectedAppointment({
+      id: appointmentTypes[0].id,
+      name: appointmentTypes[0].name,
+     });
     }
+   } catch (error) {
+    console.error('Error fetching appointment types:', error);
+   }
   };
 
-  return (
-    <div className={appointmentStyles.buttons_header}>
-      {appointmentTypes.map(appointmentType => {
-        if (['replacement_junior_group', 'appointed_junior_group'].includes(appointmentType.name))
-          return <></>;
-        return (
-          <button
-            key={appointmentType.id}
-            onClick={() => {
-              if (['free', 'universal'].includes(appointmentType.name))
-                setSelectedAppointment({name: appointmentType.name, id: appointmentType.id});
-            }}
-            className={`${appointmentStyles.type_selector} ${
-              appointmentStyles.type_selector__borders
-            } ${appointmentStyles[`type_selector__${appointmentType.name}`]}`}>
-            {translateAppointmentTypeName(appointmentType.name)}
-          </button>
-        );
-      })}
-      {/* <div className={styles.teacherInfo}>Mentor: {user?.name}</div> */}
-    </div>
-  );
+  fetchAppointmentType();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+ }, []);
+
+ const translateAppointmentTypeName = (name) => {
+  switch (name) {
+   case 'universal':
+    return 'Universal';
+   case 'appointed_group':
+    return 'Appointed group';
+   case 'appointed_private':
+    return 'Appointed private';
+   case 'appointed_junior_group':
+    return 'Appointed jun group';
+   case 'free':
+    return 'Remove';
+   case 'replacement_group':
+    return 'Group replacement';
+   case 'replacement_private':
+    return 'Private replacement';
+   case 'replacement_junior_group':
+    return 'Jun group replacement';
+   default:
+    return name;
+  }
+ };
+
+ return (
+  <div className={appointmentStyles.buttons_header}>
+   {appointmentTypes.map((appointmentType) => {
+    if (!['free', 'universal'].includes(appointmentType.name)) return <></>;
+    return (
+     <button
+      key={appointmentType.id}
+      onClick={() => {
+       if (['free', 'universal'].includes(appointmentType.name))
+        setSelectedAppointment({ name: appointmentType.name, id: appointmentType.id });
+      }}
+      className={`${appointmentStyles.type_selector} ${appointmentStyles.type_selector__borders} ${
+       appointmentStyles[`type_selector__${appointmentType.name}`]
+      }`}
+     >
+      {translateAppointmentTypeName(appointmentType.name)}
+     </button>
+    );
+   })}
+   {/* <div className={styles.teacherInfo}>Mentor: {user?.name}</div> */}
+   <a
+    data-tooltip-id="my-tooltip"
+    data-tooltip-place="right"
+    className={appointmentStyles.tooltip__icon}
+   >
+    <span className={appointmentStyles.tooltip__icon}> ?</span>
+   </a>
+   <Tooltip id="my-tooltip" className={appointmentStyles.tooltip}>
+    {appointmentTypes.map((appointmentType) => {
+     console.log(appointmentType.name);
+     if (
+      ['free', 'universal', 'replacement_junior_group', 'appointed_junior_group'].includes(
+       appointmentType.name
+      )
+     )
+      return <></>;
+     return (
+      <div className={appointmentStyles.tooltip__container}>
+       <div
+        className={`${appointmentStyles.circle} ${
+         appointmentStyles[`type_selector__${appointmentType.name}`]
+        }`}
+       ></div>
+       {translateAppointmentTypeName(appointmentType.name)}
+      </div>
+     );
+    })}
+   </Tooltip>
+  </div>
+ );
 }
