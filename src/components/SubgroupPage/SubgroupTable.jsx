ec0e3cbbceb8dad7fc5c9ec7523b0859be591458
Sheeseburger <kekymber@gmail.com>
2024-05-06 12:11:16 +0300

    added ignore line

--- src/components/SubgroupPage/SubgroupTable.jsx
+++ src/components/SubgroupPage/SubgroupTable.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect, useState} from 'react';
 import {success} from '@pnotify/core';
 import InfiniteScroll from 'react-infinite-scroll-component';
