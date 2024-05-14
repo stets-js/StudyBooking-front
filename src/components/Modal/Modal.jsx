c29bd77bd7b6e9c9d967ad918a655c3185d4bc1a
Sheeseburger <kekymber@gmail.com>
2024-05-14 10:26:53 +0300

    removed console.log

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,7 +7,6 @@ import classNames from 'classnames';
 const modalRef = document.querySelector('#root-modal');
 const root = document.querySelector('#root');
 const Modal = props => {
-  console.log(props);
   if (props.open && props.classname_wrapper !== 'login__wrapper') {
     document.body.style.overflow = 'hidden';
     root.style.overflow = 'hidden';
