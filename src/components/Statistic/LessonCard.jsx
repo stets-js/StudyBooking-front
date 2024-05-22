d006de2aa1c5de9a1c4a4200afecab13f6f58c50
Sheeseburger <kekymber@gmail.com>
2024-05-22 11:45:48 +0300

    added subgroup name to info card

--- src/components/Statistic/LessonCard.jsx
+++ src/components/Statistic/LessonCard.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
+
 import style from './statistic.module.scss';
 import UserAvatar from '../MentorChooser/UserAvatar';
+
 export default function LessonCard({lesson}) {
   const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   return (
     <div className={style.card__wrapper}>
       <UserAvatar mentor={lesson.User}></UserAvatar>
       <div className={style.card__text__container}>
-        <div className={style.card__course__container}>
-          <span>Course: </span> <span> {lesson.SubGroup.Course.name}</span>
+        <div className={style.card__leftAlign}>
+          <span>Subgroup: {lesson.SubGroup.name}</span>
         </div>
-        <div className={style.card__topic__container}>
-          <span>Topic:</span> <span>{lesson.LessonTopic?.topic || 'No topic yet'}</span>
+        <div className={style.card__leftAlign}>
+          <span>Course: {lesson.SubGroup.Course.name}</span>
         </div>
-        <div className={style.card__schedule}>
+        <div className={style.card__leftAlign}>
+          <span>Topic: {lesson.LessonTopic?.topic || 'No topic yet'}</span>
+        </div>
+        <div className={style.card__leftAlign}>
           {weekDays[lesson.LessonSchedule.weekDay]}: {lesson.LessonSchedule.startTime} -{' '}
           {lesson.LessonSchedule.endTime}
         </div>
-
-        <div className={style.card__feedback}>
+        <div className={style.card__leftAlign}>
           Feedback: {lesson?.feedback || 'No feedback yet :('}
         </div>
       </div>
