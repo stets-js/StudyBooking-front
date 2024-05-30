1c0903f8a3d0ca5d1bea79e438146a2139d7138c
Sheeseburger <kekymber@gmail.com>
2024-05-30 15:25:58 +0300

    developed statistic page

---
+++ src/components/StatisticsPage/AmountTable.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import styles from './statistics.module.scss';
+import classNames from 'classnames';
+
+const typeTranslator = id => {
+  switch (+id) {
+    case 7:
+      return 'Group';
+    case 8:
+      return 'Private';
+    case 9:
+      return 'Replaced group';
+    case 10:
+      return 'Replaced private';
+    case 11:
+      return 'Jun group';
+    case 12:
+      return 'Replaced Jun group';
+
+    default:
+      return id;
+  }
+};
+const typeReverseTranslator = typeName => {
+  switch (typeName) {
+    case 'Group':
+      return 7;
+    case 'Private':
+      return 8;
+    case 'Replaced group':
+      return 9;
+    case 'Replaced private':
+      return 10;
+    case 'Jun group':
+      return 11;
+    case 'Replaced Jun group':
+      return 12;
+    default:
+      return typeName;
+  }
+};
+
+export default function AmountTable({typeAmounts}) {
+  const orderHeaders = headers => {
+    const order = [
+      'Group',
+      'Replaced group',
+      'Private',
+      'Replaced private',
+      'Jun group',
+      'Replaced Jun group',
+      'Total'
+    ];
+    return headers.sort((a, b) => order.indexOf(a) - order.indexOf(b));
+  };
+  const headers = orderHeaders([...Object.keys(typeAmounts).map(type => typeTranslator(type))]);
+  return (
+    <div className={styles.table__wrapper}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {headers.map(header => (
+              <td key={header} className={classNames(styles.table__cell, styles.table__header)}>
+                {header}
+              </td>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr key={'firstAndLast'}>
+            {headers.map(header => {
+              return (
+                <>
+                  <td className={styles.table__cell}>
+                    {typeAmounts[typeReverseTranslator(header)]}
+                  </td>
+                </>
+              );
+            })}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- src/pages/Teacher/StatisticPage.jsx
+++ src/pages/Teacher/StatisticPage.jsx
@@ -1,14 +1,67 @@
 import React, {useEffect, useState} from 'react';
-import {error, success} from '@pnotify/core';
+// import {error, success} from '@pnotify/core';
+import {format} from 'date-fns';
 import {useParams} from 'react-router-dom';
 import {useSelector} from 'react-redux';
+import styles from '../../styles/teacher.module.scss';
+import {getLessonsForUser} from '../../helpers/lessons/lesson';
+import AmountTable from '../../components/StatisticsPage/AmountTable';
+import LessonsTable from '../../components/StatisticsPage/LessonsTable';
+
+const getFirstAndLastDateOfMonth = () => {
+  const now = new Date();
+  const firstDay = new Date(Date.UTC(now.getFullYear(), now.getMonth(), 1));
+  const lastDay = new Date(Date.UTC(now.getFullYear(), now.getMonth() + 1, 0));
+
+  return {start: format(firstDay, 'yyyy-MM-dd'), end: format(lastDay, 'yyyy-MM-dd')};
+};
 
 export default function StatisticPage() {
   const {teacherId} = useParams() || null;
   let userId = useSelector(state => state.auth.user.id);
   if (teacherId) userId = teacherId; //case when admin is logged in and wants to see another teachers schedule
-  const [lessons, setLessons] = useState();
-  const fetchData = async () => {};
 
-  return <>Statistic</>;
+  const [dates, setDates] = useState(getFirstAndLastDateOfMonth());
+  let [lessonsByDay] = useState({});
+  let [typeAmounts, setTypeAmounts] = useState({
+    // for content of constants look at typeTranslater
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0,
+    11: 0,
+    12: 0,
+    Total: 0
+  });
+  const fetchData = async () => {
+    const data = await getLessonsForUser({
+      mentorId: userId,
+      startDateLesson: dates.start,
+      endDateLesson: dates.end
+    });
+    setTypeAmounts(prev => {
+      return {...prev, Total: data.results};
+    });
+    data.data.forEach(lesson => {
+      setTypeAmounts(prev => {
+        return {...prev, [lesson.appointmentTypeId]: prev[lesson.appointmentTypeId] + 1};
+      });
+
+      if (lessonsByDay[lesson.date]) {
+        lessonsByDay[lesson.date] = [...lessonsByDay[lesson.date], lesson];
+      } else {
+        lessonsByDay[lesson.date] = [lesson];
+      }
+    });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <AmountTable typeAmounts={typeAmounts}></AmountTable>
+      <LessonsTable lessonsByDay={lessonsByDay}></LessonsTable>
+    </>
+  );
 }
