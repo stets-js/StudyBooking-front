1eb8ecb22559bcd6fb22a6fd0561e81dd553ae41
Sheeseburger <kekymber@gmail.com>
2024-09-11 15:07:39 +0300

    сleaned up imports

--- src/pages/Admin/SpreadsheetPage.jsx
+++ src/pages/Admin/SpreadsheetPage.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import {addBorders, resizeTable, updateTable} from '../../helpers/spreadsheet/spreadsheet';
-import {error, success} from '@pnotify/core';
-import styles from '../../styles/teacher.module.scss';
-import {Link} from 'react-router-dom';
-import EditButton from '../../components/Buttons/Edit';
 import TableHeader from '../../components/TableComponent/TableHeader';
 import TableBody from '../../components/Spreedsheets/Tbody';
 
--- src/pages/Teacher/ReportPage.jsx
+++ src/pages/Teacher/ReportPage.jsx
@@ -1,18 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 
-import {useParams} from 'react-router-dom';
-import {useSelector} from 'react-redux';
 import ReportsTable from '../../components/QCManager/ReportsTable';
-import {getReports} from '../../helpers/manager/qcmanager';
 
 export default function ReportPage() {
-  const [reports, setReports] = useState([]);
-  const [offsetData, setOffsetData] = useState({
-    total: 0,
-    limit: 20,
-    offset: 0
-  });
-
   return (
     <>
       <ReportsTable teacherPage></ReportsTable>
